04aef4164dff2cd4dcaa3543f608d4c389f4c139
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-12-28 22:54:37 +0900

    표준화된 규격에맞게 생산, 생성자를 통한 의존성주입

--- src/main/java/expert001_01/Car.java
+++ src/main/java/expert001_01/Car.java
@@ -2,7 +2,7 @@ package expert001_01;
 
 public class Car {
     Tire tire;
-
+    //car가 구체적으로 KoreaTire를 생산할지, AmericaTire를 생산할지 결정
     public Car(){
         tire = new KoreaTire();
         //tire = new AmericaTire();
--- src/main/java/expert001_02/Car.java
+++ src/main/java/expert001_02/Car.java
@@ -4,10 +4,11 @@ public class Car {
     Tire tire;
 
     public Car(Tire tire){
-        //new가 사라지고 생성자에 인자주가
+        //new가 사라지고 생성자에 인자추가
         this.tire = tire;
     }
-
+    //의존성 주입을 할 경우 Car는 Tire인터페이스를 구현한 어떤객체가 들어오기만 하면 작동한다.
+    // 곧 확장성도 좋아진다. 다른 타이어 브랜드들이 Tire인터페이스를 구현한다면 Car.java코드 변경없이 사용가능.
     public String getTireBrand(){
         return "장착된 타이어: "+ tire.getBrand();
     }
