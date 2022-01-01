0528efa4caece03ac64146b45e2846fcac2a57c9
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-01-01 22:16:14 +0900

    xml사용

---
+++ src/test/java/expert002/CarTest.java
@@ -0,0 +1,29 @@
+package expert002;
+
+import expert001_03.AmericaTire;
+import expert001_03.Car;
+import expert001_03.KoreaTire;
+import expert001_03.Tire;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CarTest {
+
+    @Test
+    public void 자동차_코리아타이어_장착_타이어브랜드_테스트(){
+        Tire tire = new KoreaTire();
+        Car car1 = new Car();
+        car1.setTire(tire);
+
+        assertEquals("장착된 타이어: 코리아 타이어", car1.getTireBrand());
+    }
+
+    @Test
+    public void 자동차_미국타이어_장착_타이어브랜드_테스트(){
+        Tire tire = new AmericaTire();
+        Car car2 = new Car();
+
+        assertEquals("장착된 타이어: 미국 타이어",car2.getTireBrand());
+    }
+}
